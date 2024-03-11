cc224915ddbd0468494c905733231608e8b101e9
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-11 05:51:54 +0200

    Update base_model.py

--- models/base_model.py
+++ models/base_model.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
-"""Defines the BaseModel class."""
+"""Defining BaseModel class."""
 import models
 from uuid import uuid4
 from datetime import datetime
 
 
 class BaseModel:
-    """Represents the BaseModel of the HBnB project."""
+    """Representing BaseModel of BnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Initialize a new BaseModel.
+        """Initializing a new BaseModel.
         Args:
             *args (any): Unused.
             **kwargs (dict): Key/value pairs of attributes.
@@ -28,13 +28,13 @@ class BaseModel:
             models.storage.new(self)
 
     def save(self):
-        """Update updated_at with the current datetime."""
+        """Updating updated_at with current date time."""
         self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Return the dictionary of the BaseModel instance.
-        Includes the key/value pair __class__ representing
+        """Returning the dictionary of BaseModel instance.
+        Including the key/value pair __class__
         the class name of the object.
         """
         rdict = self.__dict__.copy()
@@ -44,6 +44,6 @@ class BaseModel:
         return rdict
 
     def __str__(self):
-        """Return the print/str representation of the BaseModel instance."""
+        """Returning the print/str representation of BaseModel instance."""
         clname = self.__class__.__name__
         return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
