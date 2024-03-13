417b6fc9a4f7e6a4f605521025e53aa0c2e3b37a
AhmedNasserxd <144343880+AhmedNasserxd@users.noreply.github.com>
2024-03-13 05:43:09 +0200

    Update test_state.py

--- tests/test_models/test_state.py
+++ tests/test_models/test_state.py
@@ -1,5 +1,11 @@
 #!/usr/bin/python3
+"""Defines unittests for models/state.py.
 
+Unittest classes:
+    TestState_instantiation
+    TestState_save
+    TestState_to_dict
+"""
 import unittest
 import os
 import pep8
